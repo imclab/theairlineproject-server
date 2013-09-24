18e7a86cdea58c62d788104f244ffdc1df014c4e
Peter Morgan <pete@daffodil.uk.com>
2013-09-24 02:48:24 +0100

    facility

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-TheAirlineProject -  server
+The Airline Project -  server
 ==================================
 
 This is an experiment to create the http://theairlineproject.com but
@@ -6,9 +6,22 @@ as a web application.
 
 For fun, gonna give go a go ;-)
 
+Some documentation (doxygen) for the C# project is at
+* http://theairlineproject.freeflightsim.org/
+
+Initial goals are 
+* just to create some structs to laod xml
+* serialise to jsaon and http ajax request
+* What database do we use ???
+
 # Wanted #
 
 Go developers.. 
+
+
+
+
+
 
 
 
--- go-airline/airline/facility.go
+++ go-airline/airline/facility.go
@@ -3,9 +3,25 @@
 package airline
 
 
+/*
+ < airlinefacilities> *
+ <airlinefacility uid="100" price="100000" fromyear="1970" monthlycost="10000">
+ <level service="0" luxury="10">
+ </level>
+ <translations>
+ <en-US name="Frequent Flyer Program" shortname="Flyer" />
+ <de-DE name="Werbemaßnahme wechselnde Broschüre" shortname="Broschüre" />
+ <en-GB name="Frequent Flyer Program" shortname="Flyer" />
+ <nl-NL name="Frequent flyer-programma" shortname="Passagier" />
+ <zh-CN name="飞行常客奖励计划" shortname="常客" />
+ <da-DK name="Loyalitetsprogram" shortname="Flyer" />
+ <es-ES name="Programa de Viajero Frecuente" shortname="Folleto" />
+ </translations>
+ </airlinefacility>
+ */
 
 //
-type Facility struct{
+type AirlineFacility struct{
 	
 	Uid uint `json:"uid"` 
 	
